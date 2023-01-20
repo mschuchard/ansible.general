b8d35bf63757eaf370f3b6796897449475499a94
Matt Schuchard <grapheneowns@gmail.com>
2023-01-20 11:50:54 -0500

    add ansible to packer conversion function

--- plugins/module_utils/packer.py
+++ plugins/module_utils/packer.py
@@ -39,7 +39,7 @@ ARGS_MAP: Final[dict[str, dict[str, str]]] = dict({
 })
 
 
-def packer_cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, target_dir: str = Path.cwd()) -> list[str]:
+def cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, target_dir: str = Path.cwd()) -> list[str]:
     """constructs a list representing the packer command to execute"""
     # verify command
     if action not in FLAGS_MAP:
@@ -75,3 +75,31 @@ def packer_cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, tar
 
     # otherwise error if it does not exist (possible to target file or dir)
     raise RuntimeError(f"Targeted directory or file does not exist: {target_dir}")
+
+
+def ansible_to_packer(args: dict) -> str:
+    """converts ansible types and syntax to packer types and formatting"""
+    # in this function args dict is mutatable pseudo-reference and also returned
+    # iterate through ansible module argument
+    for arg, arg_value in args.items():
+        # list[str] to comma-delimited string
+        if arg in ['except', 'only']:
+            args[arg] = arg_value.join(',')
+        # list[dict[str, str]] to "key=value" string, TODO: finish
+        elif arg in ['var']:
+            args[arg] = f"{arg_value}={arg_value}"
+        # list[str] to ?, TODO: finish
+        elif arg in ['var_file']:
+            args[arg] = arg_value
+        # int to str
+        elif arg in ['parallel_builds']:
+            args[arg] = str(arg_value)
+        # validate on_error arg value
+        elif arg in ['on_error']:
+            if arg_value not in ['cleanup', 'abort', 'ask', 'run-cleanup-provisioner']:
+                raise RuntimeError(f"Unsupported on error argument value specified: {arg_value}")
+        # unsupported argument
+        else:
+            raise RuntimeError(f"Unsupported Packer arg specified: {arg}")
+
+    return args
--- tests/unit/plugins/module_utils/test_packer.py
+++ tests/unit/plugins/module_utils/test_packer.py
@@ -7,40 +7,50 @@ import pytest
 from mschuchard.general.plugins.module_utils import packer
 
 
-def test_packer_cmd_errors():
-    """test various packer_cmd errors"""
+def test_cmd_errors():
+    """test various cmd errors"""
     # test fails on unsupported action
     with pytest.raises(RuntimeError, match='Unsupported Packer action attempted: foo'):
-        packer.packer_cmd(action='foo')
+        packer.cmd(action='foo')
 
     # test fails on unknown flag
     with pytest.raises(RuntimeError, match='Unsupported Packer flag specified: foo'):
-        packer.packer_cmd(action='init', flags=['foo'])
+        packer.cmd(action='init', flags=['foo'])
 
     # test fails on unknown arg
     with pytest.raises(RuntimeError, match='Unsupported Packer arg specified: foo'):
-        packer.packer_cmd(action='validate', args={'foo': 'bar'})
+        packer.cmd(action='validate', args={'foo': 'bar'})
 
-    # test fails on action without args
+    # test fails on specifying args for action without corresponding args
     with pytest.raises(RuntimeError, match='Unsupported Packer arg specified: foo'):
-        packer.packer_cmd(action='init', args={'foo': 'bar'})
+        packer.cmd(action='init', args={'foo': 'bar'})
 
     # test fails on nonexistent target_dir
     with pytest.raises(RuntimeError, match='Targeted directory or file does not exist: /1234567890'):
-        packer.packer_cmd(action='init', target_dir='/1234567890')
+        packer.cmd(action='init', target_dir='/1234567890')
 
 
-def test_packer_cmd():
-    """test various packer_cmd returns"""
-
+def test_cmd():
+    """test various cmd returns"""
     # test init with no flags and no args
-    assert packer.packer_cmd(action='init', target_dir='/home') == ['packer', 'init', '-machine-readable', '-color=false', '/home']
+    assert packer.cmd(action='init', target_dir='/home') == ['packer', 'init', '-machine-readable', '-color=false', '/home']
 
     # test fmt with check flag and no args
-    assert packer.packer_cmd(action='fmt', flags=['check'], target_dir='/home') == ['packer', 'fmt', '-machine-readable', '-color=false', '-check', '/home']
+    assert packer.cmd(action='fmt', flags=['check'], target_dir='/home') == ['packer', 'fmt', '-machine-readable', '-color=false', '-check', '/home']
 
     # test validate with default target_dir, no flags, only and var args
-    assert packer.packer_cmd(action='validate', args={'only': 'null.this,null.that', 'var': 'foo=bar'}) == ['packer', 'validate', '-machine-readable', '-color=false', '-only=null.this,null.that', '-var foo=bar', Path.cwd()]
+    assert packer.cmd(action='validate', args={'only': 'null.this,null.that', 'var': 'foo=bar'}) == ['packer', 'validate', '-machine-readable', '-color=false', '-only=null.this,null.that', '-var foo=bar', Path.cwd()]
 
     # test build with force and debug flags, and parallel builds args
-    assert packer.packer_cmd(action='build', flags=['debug', 'force'], args={'parallel_builds': '1'}, target_dir='/home') == ['packer', 'build', '-machine-readable', '-color=false', '-debug', '-force', '-parallel-builds=1', '/home']
+    assert packer.cmd(action='build', flags=['debug', 'force'], args={'parallel_builds': '1'}, target_dir='/home') == ['packer', 'build', '-machine-readable', '-color=false', '-debug', '-force', '-parallel-builds=1', '/home']
+
+
+def test_ansible_to_packer_errors():
+    """test various ansible_to_packer errors"""
+    # test fails on unsupported on error argument value
+    with pytest.raises(RuntimeError, match='Unsupported on error argument value specified: foo'):
+        packer.ansible_to_packer(args={'on_error': 'foo'})
+
+    # test fails on unsupported on error argument value
+    with pytest.raises(RuntimeError, match='Unsupported Packer arg specified: foo'):
+        packer.ansible_to_packer(args={'foo': 'bar'})
