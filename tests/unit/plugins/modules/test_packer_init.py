4108cd262b4bc51a423cb8e89c6ddfbbef164f0f
Matt Schuchard <grapheneowns@gmail.com>
2023-02-06 11:47:40 -0500

    finish packer init unit tests

--- tests/unit/plugins/modules/test_packer_init.py
+++ tests/unit/plugins/modules/test_packer_init.py
@@ -20,3 +20,34 @@ def test_packer_init_defaults(capfd):
     assert info['rc'] == 1
     assert 'init' in info['cmd']
     assert 'ui,error,Error: Could not find any config file in' in info['stdout']
+
+
+def test_packer_init_config(capfd):
+    """test packer init with config"""
+    utils.set_module_args({'config_dir': '/tmp'})
+    with pytest.raises(SystemExit, match='1'):
+        packer_init.main()
+
+    stdout, stderr = capfd.readouterr()
+    assert not stderr
+
+    info = json.loads(stdout)
+    print(info)
+    assert info['rc'] == 1
+    assert '/tmp' in info['cmd']
+    assert 'ui,error,Error: Could not find any config file in /tmp' in info['stdout']
+
+
+def test_packer_init_upgrade(capfd):
+    """test packer init with upgrade"""
+    utils.set_module_args({'upgrade': True})
+    with pytest.raises(SystemExit, match='1'):
+        packer_init.main()
+
+    stdout, stderr = capfd.readouterr()
+    assert not stderr
+
+    info = json.loads(stdout)
+    assert info['rc'] == 1
+    assert '-upgrade' in info['cmd']
+    assert 'ui,error,Error: Could not find any config file in' in info['stdout']
