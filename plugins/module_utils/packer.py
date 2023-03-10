6c2d3f81b41ca28d93020049a8e0dc212bff5d04
Matt Schuchard <grapheneowns@gmail.com>
2023-03-10 11:00:47 -0500

    finish fixing and updating for var and var_file type conversion

--- plugins/module_utils/packer.py
+++ plugins/module_utils/packer.py
@@ -1,7 +1,7 @@
 """packer module utilities"""
 __metaclass__ = type
 
-from typing import Final
+from typing import Union, Final
 from pathlib import Path
 
 
@@ -27,8 +27,8 @@ ARGS_MAP: Final[dict[str, dict[str, str]]] = dict({
         'only': '-only=',
         'on_error': '-on-error=',
         'parallel_builds': '-parallel-builds=',
-        'var': '-var ',
-        'var_file': '-var-file='
+        'var': '',
+        'var_file': ''
     },
     'validate': {
         'excepts': '-except=',
@@ -39,7 +39,7 @@ ARGS_MAP: Final[dict[str, dict[str, str]]] = dict({
 })
 
 
-def cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, target_dir: Path = Path.cwd()) -> list[str]:
+def cmd(action: str, flags: set[str] = [], args: dict[str, Union[str, list[str]]] = {}, target_dir: Path = Path.cwd()) -> list[str]:
     """constructs a list representing the packer command to execute"""
     # verify command
     if action not in FLAGS_MAP:
@@ -64,9 +64,17 @@ def cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, target_dir
     # not all actions have args, so return empty dict by default to shortcut to RuntimeError for unpported arg if arg specified for action without args
     action_args_map: dict[str, str] = ARGS_MAP.get(action, {})
     for arg, arg_value in args.items():
+        # verify this is a valid action argument
         if arg in action_args_map:
-            # add packer arg from corresponding module arg in ARGS
-            command.append(f"{action_args_map[arg]}{arg_value}")
+            # if the arg value is a str, then append the value interpolated with the arg name from the dict to the command
+            if isinstance(arg_value, str) and len(action_args_map[arg]) > 0:
+                command.append(f"{action_args_map[arg]}{arg_value}")
+            # if the arg value is a list, then extend the command with the values because they are already formatted correctly
+            elif isinstance(arg_value, list) and len(action_args_map[arg]) == 0:
+                command.extend(arg_value)
+            # if the arg value is some other type, or there was some unexpected mismatch between the arg name expecting a str/list and the other being provided, then something has gone wrong
+            else:
+                raise RuntimeError(f"Unexpected issue with argument name '{arg}' and argument value '{arg_value}'")
         else:
             # unsupported arg specified
             raise RuntimeError(f"Unsupported Packer arg specified: {arg}")
@@ -79,7 +87,7 @@ def cmd(action: str, flags: set[str] = [], args: dict[str, str] = {}, target_dir
     raise RuntimeError(f"Targeted directory or file does not exist: {target_dir}")
 
 
-def ansible_to_packer(args: dict) -> dict[str, str]:
+def ansible_to_packer(args: dict) -> dict[str, Union[str, list[str]]]:
     """converts ansible types and syntax to packer types and formatting for arguments only"""
     # in this function args dict is mutatable pseudo-reference and also returned
     # iterate through ansible module argument
