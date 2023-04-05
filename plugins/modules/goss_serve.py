207f203b766b28f7846598c760b0331bd306dcd7
Matt Schuchard <grapheneowns@gmail.com>
2023-04-05 15:35:33 -0400

    prototype goss_serve module

--- plugins/modules/goss_serve.py
+++ plugins/modules/goss_serve.py
@@ -75,7 +75,65 @@ TODO
 
 def run_module() -> None:
     """primary function for goss serve module"""
-    
+    # instanstiate ansible module
+    module = AnsibleModule(
+        argument_spec=dict(
+            endpoint=dict(type='str', required=False, default=''),
+            format=dict(type='str', required=False, default=''),
+            gossfile=dict(type='str', required=False, default=Path.cwd()),
+            port=dict(type='int', required=False, default=0),
+            vars=dict(type='str', required=False, default=Path.cwd())
+        ),
+        supports_check_mode=True
+    )
+
+    # initialize
+    changed: bool = False
+    endpoint: str = module.params.get('endpoint')
+    the_format: str = module.params.get('format')
+    the_vars: Path = Path(module.params.get('vars'))
+    port: int = module.params.get('port')
+    gossfile: Path = Path(module.params.get('gossfile'))
+    cwd: str = str(Path.cwd())
+    if gossfile != Path.cwd():
+        cwd = str(gossfile.parent)
+
+    # check args
+    args: dict = {}
+    if len(endpoint) > 0:
+        args.update({'endpoint': endpoint})
+    if len(the_format) > 0:
+        args.update({'format': the_format})
+    if port > 0:
+        args.update({'port': port})
+    if the_vars != Path.cwd():
+        args.update({'vars': str(the_vars)})
+
+    # determine goss command
+    command: str = goss.cmd(action='serve', args=args, gossfile=gossfile)
+
+    # exit early for check mode
+    if module.check_mode:
+        module.exit_json(changed=False, command=command)
+
+    # execute goss
+    return_code: int
+    stdout: str
+    stderr: str
+    return_code, stdout, stderr = module.run_command(command, cwd=cwd)
+
+    # check symbolic idempotence
+    if len(stdout) > 0:
+        changed = True
+
+    # post-process
+    if return_code == 0:
+        module.exit_json(changed=changed, stdout=stdout, stderr=stderr, command=command)
+    else:
+        module.fail_json(
+            msg=stderr.rstrip(), return_code=return_code, cmd=command,
+            stdout=stdout, stdout_lines=stdout.splitlines(),
+            stderr=stderr, stderr_lines=stderr.splitlines())
 
 
 def main() -> None:
