6692a4e8e55c9a3a4a8e4274cf8e37e56311aa24
Matt Schuchard <grapheneowns@gmail.com>
2024-08-05 12:02:50 -0400

    add two final use case unit tests

--- tests/unit/plugins/modules/test_terraform_init.py
+++ tests/unit/plugins/modules/test_terraform_init.py
@@ -35,3 +35,36 @@ def test_terraform_init_config(capfd):
     info = json.loads(stdout)
     assert f"-chdir={str(utils.fixtures_dir())}" in info['command']
     assert 'Terraform has been successfully initialized!' in info['stdout']
+
+
+def test_terraform_init_upgrade_backend(capfd):
+    """test terraform init with upgrade"""
+    utils.set_module_args({'upgrade': True, 'backend': False, 'config_dir': str(utils.fixtures_dir())})
+    with pytest.raises(SystemExit, match='0'):
+        terraform_init.main()
+
+    stdout, stderr = capfd.readouterr()
+    assert not stderr
+
+    info = json.loads(stdout)
+    assert '-upgrade' in info['command']
+    assert f"-chdir={str(utils.fixtures_dir())}" in info['command']
+    assert 'Terraform has been successfully initialized!' in info['stdout']
+
+def test_terraform_init_multiple_args(capfd):
+    """test terraform init with multiple arguments and a flag"""
+    utils.set_module_args({'migrate_state': True, 'backend_config': [f"{str(utils.fixtures_dir())}/config.tf", {'scheme': 'https'}], 'plugin_dir': [str(utils.fixtures_dir()), '/tmp']})
+    with pytest.raises(SystemExit, match='0'):
+        terraform_init.main()
+
+    stdout, stderr = capfd.readouterr()
+    assert not stderr
+
+    info = json.loads(stdout)
+    assert '-migrate-state' in info['command']
+    assert f"-backend-config={str(utils.fixtures_dir())}/config.tf" in info['command']
+    print(info['command'])
+    assert "-backend-config='scheme=https'" in info['command']
+    #assert f"-plugin-dir={str(utils.fixtures_dir())}" in info['command']
+    #assert '-plugin-dir=/tmp' in info['command']
+    assert 'Terraform initialized in an empty directory!' in info['stdout']
