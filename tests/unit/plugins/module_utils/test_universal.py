deee7803168c8022d113f2b28a708d6218249ead
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-03-20 10:49:35 -0400

    truly robustly handle complex type inputs for hcl2

--- tests/unit/plugins/module_utils/test_universal.py
+++ tests/unit/plugins/module_utils/test_universal.py
@@ -33,7 +33,10 @@ def test_action_flags_command():
 def test_vars_converter():
     """test ansible vars param to hashi cli converter"""
     # test accurate vars conversion
-    assert universal.vars_converter([{'var1': 'value1'}, {'var2': 'value2'}, {'var3': 'value3'}]) == ['-var', 'var1=value1', '-var', 'var2=value2', '-var', 'var3=value3']
+    assert universal.vars_converter([{'var1': 'value1'}, {'var2': 'value2'}, {'var3': 'value3'}]) == ['-var', 'var1=\'value1\'', '-var', 'var2=\'value2\'', '-var', 'var3=\'value3\'']
+
+    # test with complex types
+    assert universal.vars_converter([{'var1': ['value1', 'value2']}, {'var2': {'foo': 'bar', 'baz': 'bot'}}]) == ['-var', 'var1=\'["value1","value2"]\'', '-var', 'var2=\'{"foo":"bar","baz":"bot"}\'']
 
 
 def test_var_files_converter():
