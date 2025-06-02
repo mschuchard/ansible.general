de4414cfc669100b32c75b6a39b959ef7bf36bb0
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-06-02 16:15:49 -0400

    add unit tests for faas module util errors

--- tests/unit/plugins/module_utils/test_faas.py
+++ tests/unit/plugins/module_utils/test_faas.py
@@ -10,6 +10,18 @@ def test_faas_cmd_errors():
     with pytest.raises(RuntimeError, match='Unsupported FaaS action attempted: foo'):
         faas.cmd(action='foo')
 
+    # test warns on unknown flag, and discards unknown flag
+    with pytest.warns(RuntimeWarning, match='Unsupported flag specified: foo'):
+        assert faas.cmd(action='build', flags={'foo'}) == ['faas-cli', 'build']
+
+    # test warns on unknown arg, and discards unknown arg
+    with pytest.warns(RuntimeWarning, match='Unsupported FaaS arg specified: foo'):
+        assert faas.cmd(action='logs', args={'foo': 'bar'}) == ['faas-cli', 'logs']
+
+    # test warns on specifying args for action without corresponding args, and discards offending arg
+    with pytest.warns(RuntimeWarning, match='Unsupported FaaS arg specified: foo'):
+        assert faas.cmd(action='remove', args={'foo': 'bar'}) == ['faas-cli', 'remove']
+
     # test fails on nonexistent function file
     with pytest.raises(FileNotFoundError, match='Function config file does not exist or is invalid: /faas.yaml'):
         faas.cmd(action='build', args={'config_file': '/faas.yaml'})
@@ -18,6 +30,18 @@ def test_faas_cmd_errors():
     with pytest.warns(SyntaxWarning, match='Specified YAML or JSON file does not contain valid YAML or JSON: .gitignore'), pytest.raises(ValueError):
         faas.cmd(action='deploy', args={'config_file': '.gitignore'})
 
+    # test fails on soft parameter with invalid value
+    with pytest.raises(ValueError, match='The "sort" parameter must be either "name" or "invocations"'):
+        faas.cmd(action='list', args={'sort': 'invalid'})
+
 
 def test_faas_cmd():
     """test various cmd returns"""
+
+
+def test_ansible_to_faas_errors():
+    """test various ansible_to_faas errors"""
+
+
+def test_ansible_to_faas():
+    """test various ansible_to_faas returns"""
