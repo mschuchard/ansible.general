175102c820b0cac6cd0a1bc559d9102a45b21b9b
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2026-01-19 15:54:34 -0500

    add unit tests for expanded puppet module utils functionality

--- tests/unit/plugins/module_utils/test_puppet.py
+++ tests/unit/plugins/module_utils/test_puppet.py
@@ -22,17 +22,28 @@ def test_puppet_cmd_errors():
     # test warns on specifying args for action without corresponding args, and discards offending arg
     with (
         pytest.warns(RuntimeWarning, match='Unsupported Puppet arg specified: foo'),
-        pytest.raises(FileNotFoundError, match='Puppet manifest is not a file or does not exist: .+/mschuchard/general'),
     ):
-        puppet.cmd(action='apply', args={'foo': 'bar'})
+        puppet.cmd(action='apply', args={'foo': 'bar'}, manifest=Path('/etc/group'))
 
     # test fails on directory specified for manifest
     with pytest.raises(FileNotFoundError, match='Puppet manifest is not a file or does not exist: /home'):
         puppet.cmd(action='apply', manifest=Path('/home'))
 
+    # test fails on nonexistent catalog file
+    with pytest.raises(FileNotFoundError, match='Puppet catalog file does not exist or is invalid: /nonexistent.json'):
+        puppet.cmd(action='apply', catalog=Path('/nonexistent.json'))
+
+    # test fails on function file with invalid yaml content
+    with pytest.warns(SyntaxWarning, match='Specified YAML or JSON file does not contain valid YAML or JSON: .gitignore'), pytest.raises(ValueError):
+        puppet.cmd(action='apply', catalog=Path('.gitignore'))
+
     # test fails on invalid server_port
     with pytest.raises(ValueError, match='Puppet server_port value must be between 1 and 65535: 0'):
         puppet.cmd(action='agent', args={'server_port': 0})
+
+    # test fails when no manifest, execute, or catalog provided for apply
+    with pytest.raises(RuntimeError, match='One of manifest, execute, or catalog must be provided for apply action'):
+        puppet.cmd(action='apply')
 
 
 def test_puppet_cmd():
@@ -42,6 +53,9 @@ def test_puppet_cmd():
 
     # test apply with test and noop
     assert set(puppet.cmd(action='apply', flags={'test', 'no_op'}, manifest=Path('/etc/group'))) == {'puppet', 'apply', '-t', '--noop', '/etc/group'}
+
+    # test apply with execute
+    assert puppet.cmd(action='apply', execute='notify { "test": }') == ['puppet', 'apply', '-e', 'notify { "test": }']
 
     # test agent with certname and serverport
     assert puppet.cmd(action='agent', args={'certname': 'example.domain', 'server_port': 1234, 'waitforcert': 60}) == [
